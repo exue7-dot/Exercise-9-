f8762d4e8d1a2ce411f8d4e7f51a97e60106e773
Evan Xue <exue7@terpmail.umd.edu>
2025-11-09 23:25:22 -0500

    Updates the python file with another print function that prints out a goodbye

--- Exercise 9.py
+++ Exercise 9.py
@@ -1,6 +1,3 @@
 print("Hello world!")
-<<<<<<< HEAD
-print ("Have a great day!")
-=======
 print("May everyday of your life be filled with luck!")
->>>>>>> 6be68a5a2765554f59be94566060e1c52ca07ffd
+print ("Have a great day and enjoy life!")
